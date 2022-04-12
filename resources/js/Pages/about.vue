00845404eeeed0351f2139a418d13950b8de17a0
mesaque <mesaque1212@gmail.com>
2022-04-12 12:19:38 -0300

    Passando raiva_2

---
+++ resources/js/Pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <layout>
+        <h1>Eu sou a página about</h1>
+    </layout>
+</template>
+
+<script>
+import Layout from '../Layout.vue'
+export default {
+  components: { Layout },
+    name: 'about'
+}
+</script>
+
+<style>
+</style>
