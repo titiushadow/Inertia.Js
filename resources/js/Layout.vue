46d4f1978b99f033d41c4e35a37f3fb225d5bf85
mesaque <mesaque1212@gmail.com>
2022-04-12 12:20:01 -0300

    Criando Layout

---
+++ resources/js/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <header>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+        </header>
+
+        <article>
+            <slot />
+        </article>
+    </main>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    name: "Layout",
+    components: {
+        Link,
+    },
+};
+</script>
