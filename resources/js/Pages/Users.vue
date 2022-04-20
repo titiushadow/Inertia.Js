152dc247351d4f3af7c1adfd05da0ba631a73458
mesaque <mesaque1212@gmail.com>
2022-04-20 11:51:10 -0300

    Importando componente layout para Users

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,24 +1,32 @@
 <template>
-  <h1>Users</h1>
-  <Nav />
+  <Layout>
+    <h1 class="text-3xl font-bold">
+      Users
+    </h1>
 
-  <div>
-      <p>A hora correta é {{ time }}</p>
+    <div style="margin-top: 400px">
+      <p>The current time is {{ time }}.</p>
 
-      <Link href="/users" class="text-blue-500 preserve-scroll">
+      <Link
+        href="/users"
+        class="text-blue-500 preserve-scroll"
+      >
          Refresh
       </Link>
   </div>
+</Layout>
+
 </template>
 
 <script>
 
-import Nav from "../Shared/Nav.vue";
+
 import { Link } from "@inertiajs/inertia-vue3"
+import Layout from "../Shared/Layout";
 
 export default {
    name: 'home',
-    components:{ Nav, Link },
+    components: { Layout, Link },
     props: { time: String }
 };
 </script>
