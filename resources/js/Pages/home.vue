1d706fdd6e71c2a45d2a254954ce661d6a96c973
mesaque <mesaque1212@gmail.com>
2022-04-19 22:12:06 -0300

    Fazendo a importação do componente Nav para home

--- resources/js/Pages/home.vue
+++ resources/js/Pages/home.vue
@@ -1,21 +1,29 @@
 <template>
     <div>
       <h1>Hello world {{ name }}</h1>
+
+<!-- Pegando array que esta nas rotas através do props  -->
       <p>Uso os seguintes frameworks:</p>
         <ul>
             <li v-for="framework of frameworks" v-text="framework"></li>
         </ul>
     </div>
+
+<Nav />
+
 </template>
 
 <script>
+import Nav from "../Shared/Nav.vue";
+
 export default {
     name: 'home',
+    components:{ Nav },
     props: {
         name: String,
         frameworks: Array
     },
-}
+};
 </script>
 
 <style>
