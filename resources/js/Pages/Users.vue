633ae97b839d9dd245adffc58022f2773c6b6bd9
mesaque <mesaque1212@gmail.com>
2022-04-19 22:11:21 -0300

    Importando o componente Nav para o componente Users

---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Users</h1>
+  <Nav />
+</template>
+
+<script>
+
+import Nav from "../Shared/Nav.vue";
+
+export default {
+   name: 'home',
+    components:{ Nav },
+};
+</script>
+
+<style>
+
+</style>
