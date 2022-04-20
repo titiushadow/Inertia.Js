4e859a60c8e1d9af96804d14d625915846d3d8e3
mesaque <mesaque1212@gmail.com>
2022-04-19 22:10:59 -0300

    Criando o componente Nav

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+   <nav>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="users">Users</Link></li>
+          <li><Link href="settings">Settings</Link></li>
+        </ul>
+     </nav>
+</template>
+
+<script>
+
+import { Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+        Link,
+    }
+};
+</script>
+
+<style>
+
+</style>
