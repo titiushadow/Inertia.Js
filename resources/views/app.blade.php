bd9a0ebfd26251cdab06ceaa89eb738391f1927a
mesaque <mesaque1212@gmail.com>
2022-04-19 23:09:39 -0300

    Adicionando tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel_Vue</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
 <body>
-    @inertia
+    <section class="p-8">
+        @inertia
+    </section>
 </body>
 </html>
