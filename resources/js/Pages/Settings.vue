522461ef057c5b4406d9f1d05fa56261a5bd6963
mesaque <mesaque1212@gmail.com>
2022-04-19 22:11:37 -0300

    Importanto o componente Nav para Settings

---
+++ resources/js/Pages/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Settings</h1>
+  <Nav />
+</template>
+
+<script>
+
+import Nav from "../Shared/Nav.vue";
+
+export default {
+    name: 'Settings',
+    components: { Nav },
+};
+</script>
+
+<style>
+
+</style>
