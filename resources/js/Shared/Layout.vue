9182f74c9930f789e462cb4b54c3a34bd14f250e
mesaque <mesaque1212@gmail.com>
2022-04-20 11:50:20 -0300

    Criando o componente Layout Adicionando um layout no site

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+
+<section class="p-6 bg-gray-200">
+    <header class="flex justify-between">
+        <h1 class="font-bold text-lg">
+            My Website
+        </h1>
+        <Nav />
+    </header>
+</section>
+
+<section class="p-6">
+    <div class="max-w-3xl mx-auto">
+        <slot />
+    </div>
+</section>
+
+</template>
+
+<script>
+import Nav from "./Nav"
+
+export default {
+    components: { Nav },
+
+}
+</script>
+
+<style>
+
+</style>
