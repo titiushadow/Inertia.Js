b028a056742b5da869a74d3ecba5578e6429a9fd
mesaque <mesaque1212@gmail.com>
2022-04-19 23:26:57 -0300

    Preserve-Scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,18 +1,34 @@
 <template>
   <h1>Users</h1>
   <Nav />
+
+  <div>
+      <p>A hora correta é {{ time }}</p>
+
+      <Link href="/users" class="text-blue-500 preserve-scroll">
+         Refresh
+      </Link>
+  </div>
 </template>
 
 <script>
 
 import Nav from "../Shared/Nav.vue";
+import { Link } from "@inertiajs/inertia-vue3"
 
 export default {
    name: 'home',
-    components:{ Nav },
+    components:{ Nav, Link },
+    props: { time: String }
 };
 </script>
 
 <style>
+
+p {
+    font-family: bold;
+    font-size: 20px;
+    margin-top: 400px;
+}
 
 </style>
