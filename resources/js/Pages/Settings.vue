854cf9167364fc653cf0e4a73d7753c041a54781
mesaque <mesaque1212@gmail.com>
2022-04-21 17:35:12 -0300

    Adicionando dados e adicioandno props

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,8 +1,11 @@
 <template>
 <Layout>
-    <h1 class="text-3xl font-bold">
-      Settings
-    </h1>
+      <h1 class="text-3xl font-bold">Settings</h1>
+      <div class="flex items-center">
+            <p class="text-md ml-4">
+                Welcome Back, {{ name }}!
+            </p>
+      </div>
   </Layout>
 </template>
 
@@ -13,9 +16,18 @@ import Layout from "../Shared/Layout.vue"
 export default {
     name: 'Settings',
     components: { Layout },
+
+    props: {
+        name: String,
+    }
 };
 </script>
 
 <style>
+
+p {
+    font-family: bold;
+    font-size: 25px;
+}
 
 </style>
