78c0f2a6879922ce9b1739df3b6fd3a3e3340a9d
mesaque <mesaque1212@gmail.com>
2022-04-26 11:27:53 -0300

    Apagando todo conteudo de settings

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,29 +1,5 @@
 <template>
-<Layout>
       <h1 class="text-3xl font-bold">Settings</h1>
-      <div class="flex items-center">
-            <p class="text-md ml-4">
-                Welcome Back, {{ name }}!
-            </p>
-      </div>
-  </Layout>
 </template>
 
-<script setup>
-import Layout from "../Shared/Layout.vue"
 
-Layout: Layout
-
-defineProps({
-    name: String,
-});
-</script>
-
-<style>
-
-p {
-    font-family: bold;
-    font-size: 25px;
-}
-
-</style>
