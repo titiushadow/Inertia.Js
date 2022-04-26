a4072dcf24849941a470db514172aecf304750cb
mesaque <mesaque1212@gmail.com>
2022-04-26 12:23:33 -0300

    Apagando title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel_Vue</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/manifest.js') }}" defer></script>
