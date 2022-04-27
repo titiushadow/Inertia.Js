94a470eaaef301161f9516f22f700cb73adc9ce9
mesaque <mesaque1212@gmail.com>
2022-04-27 12:02:31 -0300

    tirando o import e alterando o head

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,11 +1,8 @@
 <template>
-<Head>
-     <title>My app - Settings</title>
-</Head>
+ <Head title="Settings" />
       <h1 class="text-3xl font-bold">Settings</h1>
 </template>
 
 <script setup>
-import { Head } from "@inertiajs/inertia-vue3";
 </script>
 
