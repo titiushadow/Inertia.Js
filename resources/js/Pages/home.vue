1d293e2346117ddbc660215b27abb7a0684fd17d
mesaque <mesaque1212@gmail.com>
2022-04-27 12:02:46 -0300

    adicionando coisas

--- resources/js/Pages/home.vue
+++ resources/js/Pages/home.vue
@@ -1,10 +1,12 @@
 <template>
 <Head>
-    <title>My app - Home</title>
+    <title>Home</title>
+    <meta
+     type="description"
+     content="Home Information"
+     head-key="description"
+    >
 </Head>
+
     <h1 class="text-3xl font-bold">Home</h1>
 </template>
-
-<script setup>
-import { Head } from "@inertiajs/inertia-vue3";
-</script>
