b324c5e3e5f35cbdedc37d383c6b5f2760e01eac
mesaque <mesaque1212@gmail.com>
2022-04-30 16:46:18 -0300

    Adicionando array de usuarios e lista de usuarios

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,34 +1,22 @@
 <template>
-  <Layout>
 
-  <Head title="Users"/>
+<Head title="Users" />
+    <h1 class="text-3xl font-bold">Users</h1>
 
-      <h1 class="text-3xl font-bold">Users</h1>
-      <div class="flex items-center">
-            <p class="text-md ml-4">
-                Welcome Back, {{ name }}!
-            </p>
-      </div>
-    <div style="margin-top: 400px">
-      <p>The current time is {{ time }}.</p>
-
-      <Link
-        href="/users"
-        class="text-blue-500 preserve-scroll"
-      >
-         Refresh
-      </Link>
-  </div>
-</Layout>
-
+    <ul>
+        <li
+          v-for="user in users"
+          :key="user.id"
+          v-text="user.name"
+        />
+    </ul>
 </template>
 
 
 <script setup>
 
 defineProps({
-    time: String,
-    name: String,
+    users: Array
 });
 </script>
 
