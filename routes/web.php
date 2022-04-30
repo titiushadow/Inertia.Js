c8331233a21651459b5e49ec74d2fa84a1980461
mesaque <mesaque1212@gmail.com>
2022-04-30 16:46:00 -0300

    Adicionando usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\homeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +26,7 @@ Route::get('/', function () {
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
-        'time' => now()->toTimeString(),
-        'name' => 'Mesaque'
+        'users' => User::all()
     ]);
 });
 
