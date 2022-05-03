23992496659c94663a42a40333cb2f4339db85f4
mesaque <mesaque1212@gmail.com>
2022-05-03 11:57:44 -0300

    Fazendo pequenas alterações

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,29 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
-import { InertiaProgress } from '@inertiajs/progress'
+import { createApp, h } from "vue";
+import { createInertiaApp, Link, Head } from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from "@inertiajs/progress";
 import Layout from "./Shared/Layout";
 
-
 createInertiaApp({
-  resolve:  async name => {
-  let page = (await import(`./Pages/${name}`)).default;
+  resolve: name => {
+    let page = require(`./Pages/${name}`).default;
 
-  page.layout ??= Layout
+    page.layout ??= Layout;
 
-  return page;
-
+    return page;
   },
+
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .mount(el)
       .component("Link", Link)
       .component("Head", Head)
+      .mount(el);
   },
-  title: title => `My app ${title}`
-})
+
+  title: title => `My App - ${title}`
+});
 
 InertiaProgress.init({
-    color: 'green',
-    showSpinner: 'true',
+  color: "red",
+  showSpinner: true,
 });
