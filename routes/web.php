5a054608e0d02c8a1c4e952c725584976c24b79c
mesaque <mesaque1212@gmail.com>
2022-05-16 10:21:49 -0300

    mudando o arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,18 @@
 <?php
 
-use App\Http\Controllers\homeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('login', [LoginController::class, 'create'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
+Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
 
-Route::get('/', function () {
-    return Inertia::render('home');
-});
+Route::middleware('auth')->group(function () {
+    Route::inertia('/', 'Home');
+    Route::inertia('/settings', 'Settings');
 
-Route::get('/users', function () {
-    return Inertia::render('Users', [
-        'users' => User::query()
-        ->when(Request::input('search'), function ($query, $search) {
-            $query->where('name', 'like', "%{$search}%");
-        })
-
-        ->paginate(10)
-        ->withQueryString()
-        ->through(fn($user) => [
-            'id' => $user->id,
-            'name' => $user-> name
-        ]),
-        'filters' => Request::only(['search'])
-        ]);
-    });
-
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-});
-
-Route::post('/logout', function () {
-    dd(request('foo'));
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::get('/users/create', [UsersController::class, 'create']);
+    Route::post('/users', [UsersController::class, 'store']);
 });
